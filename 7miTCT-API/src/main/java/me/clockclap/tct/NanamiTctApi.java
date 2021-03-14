8fa4097dfad5780991fefc1227d7dc90195d6ea8
ClockClap <69692349+NickStarblast@users.noreply.github.com>
2021-03-14 14:52:17 +0900

    Removed Nanami TCT Plugin.

--- 7miTCT-API/src/main/java/me/clockclap/tct/NanamiTctApi.java
+++ 7miTCT-API/src/main/java/me/clockclap/tct/NanamiTctApi.java
@@ -2,6 +2,11 @@ package me.clockclap.tct;
 
 import com.google.common.collect.UnmodifiableListIterator;
 import me.clockclap.tct.api.Utilities;
+import me.clockclap.tct.command.CommandAboutTCT;
+import me.clockclap.tct.event.ChatEvent;
+import me.clockclap.tct.event.PlayerConnectionEvent;
+import me.clockclap.tct.event.ProtectWorld;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -16,7 +21,17 @@ public final class NanamiTctApi extends JavaPlugin {
         plugin = this;
         utilities = new Utilities(this);
 
-        getLogger().info("Starting up api...");
+        plugin = this;
+        getLogger().info("Starting up...");
+        PluginManager pm = Bukkit.getServer().getPluginManager();
+
+        //Register Events
+        pm.registerEvents(new PlayerConnectionEvent(), this);
+        pm.registerEvents(new ChatEvent(), this);
+        pm.registerEvents(new ProtectWorld(), this);
+
+        //Add Commands
+        utilities.addCommand("abouttct", new CommandAboutTCT());
 
     }
 

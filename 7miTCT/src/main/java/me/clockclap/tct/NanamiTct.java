3aa6aaf56885c7ee858d2889be0fc35fff10f4a6
ClockClap <69692349+NickStarblast@users.noreply.github.com>
2021-03-14 15:12:45 +0900

    Renamed 7mi TCT API to 7mi TCT Plugin

--- 7miTCT/src/main/java/me/clockclap/tct/NanamiTct.java
+++ 7miTCT/src/main/java/me/clockclap/tct/NanamiTct.java
@@ -6,21 +6,23 @@ import me.clockclap.tct.event.ChatEvent;
 import me.clockclap.tct.event.PlayerConnectionEvent;
 import me.clockclap.tct.event.ProtectWorld;
 import org.bukkit.Bukkit;
-import org.bukkit.event.Listener;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class NanamiTct extends JavaPlugin {
 
     public static NanamiTct plugin;
+    public static Utilities utilities;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
         plugin = this;
+        utilities = new Utilities(this);
+
+        plugin = this;
         getLogger().info("Starting up...");
         PluginManager pm = Bukkit.getServer().getPluginManager();
-        Utilities util = NanamiTctApi.utilities;
 
         //Register Events
         pm.registerEvents(new PlayerConnectionEvent(), this);
@@ -28,7 +30,7 @@ public final class NanamiTct extends JavaPlugin {
         pm.registerEvents(new ProtectWorld(), this);
 
         //Add Commands
-        util.addCommand("abouttct", new CommandAboutTCT());
+        utilities.addCommand("abouttct", new CommandAboutTCT());
 
     }
 

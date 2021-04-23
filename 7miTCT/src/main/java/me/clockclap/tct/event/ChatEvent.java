bf496058e141a85a954441958756f1738d4d5050
ClockClap <i.am.clockclap@gmail.com>
2021-04-23 22:59:59 +0900

    7mi-TCTをこっちに移動 & ちょこっと修正

--- 7miTCT/src/main/java/me/clockclap/tct/event/ChatEvent.java
+++ 7miTCT/src/main/java/me/clockclap/tct/event/ChatEvent.java
@@ -12,7 +12,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
-import org.omg.CORBA.UNSUPPORTED_POLICY;
 
 import java.io.UnsupportedEncodingException;
 
